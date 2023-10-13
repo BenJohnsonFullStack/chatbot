058172341a514fd23d38fe11c4d66b71745abbbe
Ben Johnson <monk.sound.design@gmail.com>
2023-10-12 23:34:16 -0400

    message content and response

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useChat } from "ai/react";
 import Textarea from "react-textarea-autosize";
 
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
@@ -13,11 +13,39 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-neutral-800">
       {messages.length !== 0 ? (
-        <div></div>
+        <div className="pb-32 pt-5 space-y-5 w-[75%] mx-auto relative">
+          {messages.map((message) => (
+            <div key={message.id} className="w-full">
+              {message.role === "user" ? (
+                <div className="flex items-center gap-x-2">
+                  <div className="bg-gray-500 h-12 w-12 rounded-lg">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-full h-full text-white p-1"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <p className="rounded-lg p-3 w-full border-gray-500 border-2 text-sm">
+                    {message.content}
+                  </p>
+                </div>
+              ) : (
+                <div>{message.content}</div>
+              )}
+            </div>
+          ))}
+        </div>
       ) : (
         <div className="w-full flex justify-center pt-32">
           <h1 className="font-bold text-3xl">
-            Please use the input field below
+            Hello! Ask me a question in the field below...
           </h1>
         </div>
       )}
